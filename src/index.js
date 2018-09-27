9cf6aaf544d488b94f915f62fb5c13812bc5c7a6
Nikita K <dentochannel@gmail.com>
2018-09-27 16:03:53 +0300

    ver.2
    поменял цвета, сделал код чуть-чуть короче, добавил выделение точки с наибольшей скоростью изменения численности

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const vm = new Vue({
     data: {
         k: 100,
         n: 2,
-        r: 0.3
+        r: 0.3,
+        highlightedPoint: undefined
     },
     computed: {
-        // initial calculations
+        // геттер для набора значений n(t) при t=[0, 50]
         result() {
             let result = []
             for (let t = 0, n; t <= 50; t++) {
@@ -14,6 +15,23 @@ const vm = new Vue({
                 result.push(n)
             }
             return result
+        },
+        // геттер точки с наибольшей скоростью изменения графика (возвращает координату t)
+        pointToHighlight() {
+            let changeRate         // скорость изменения графика для текущей точки
+            let maxChangeRate = 0  // максимальная скорость изменения графика
+            let pointToHighlight   // координата t точки, которую необходимо выделить
+
+            for (let t = 0; t <= 50; t++) {
+                changeRate = this.calcChangeRate(t)
+                
+                if (Math.abs(changeRate) > Math.abs(maxChangeRate)) {
+                    maxChangeRate = changeRate
+                    pointToHighlight = t
+                }
+            }
+
+            return pointToHighlight
         }
     },
     methods: {
@@ -26,17 +44,67 @@ const vm = new Vue({
             return k / (1 + ((k/n)-1) * Math.pow(Math.E, (-r*t)))
         },
 
-        recalcValues() {
-            chart.data.datasets[0].data = []
+        // вычисляет значение скорости изменения графика функции в заданной точке t
+        calcChangeRate(t) {
+            const k = this.k
+            const r = this.r
+            const n = this.n
 
-            for (let t = 0, n; t <= 50; t++) {
-                n = this.calcN(t).toFixed(2)
-                chart.data.datasets[0].data.push(n)
-            }
-            
+            // Эта формула - продифференцированная функция n(t)
+            return (r*k*((k/n)-1)*Math.pow(Math.E, (-r*t)))/Math.pow((1+((k/n)-1)*Math.pow(Math.E, (-r*t))), 2)
+        },
+
+        // перерисовать график с новыми данными 
+        rerenderChart() {
+            let chartData = chart.data.datasets[0].data
+
+            // поменять текущие значения на новые
+            this.result.forEach((newValue, i) => {
+                chartData[i] = newValue
+            })
+
+            // выделить необходимую точку
+            this.highlightPoint(this.pointToHighlight)
+
             chart.update()
-        }
+        },
+
+        // визуально выделить точку, с координатой t
+        highlightPoint(t) {
+            // очистить предыдущюю выделенную точку, если такая есть
+            if (this.highlightedPoint !== undefined) this.dehighlightPoint(this.highlightedPoint)
+
+            // определение кастомных стилей для выбранной точки
+            let point = chart.getDatasetMeta(0).data[t]
+            if (point === undefined) return // выйти из функции, если точки не существует
+            point.custom = point.custom || {}
+
+            point.custom.backgroundColor = "rgb(79, 214, 178)"
+            point.custom.radius = 9
+        
+            // запомнить координату выделенной точки (чтобы в дальнейшем ее было легко "очистить")
+            this.highlightedPoint = t
+        },
+
+        // очистить кастомные стили точки с индексом t
+        dehighlightPoint(t) {
+            let point = chart.getDatasetMeta(0).data[t]
+            delete point.custom
+        },
     },
+    // следит за изменениями параметров и перерисовывает график при необходимости
+    watch: {
+        k() {
+            this.rerenderChart()
+        },
+        n() {
+            this.rerenderChart()
+        },
+        r() {
+            this.rerenderChart()
+        },
+
+    }
 })
 
 const ctx = document.getElementById('graph').getContext('2d')
@@ -47,10 +115,10 @@ var chart = new Chart(ctx, {
         datasets: [{
             data: vm.result,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'rgba(79, 214, 178, 0.2)',
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
+                'rgb(66, 178, 148)',
             ],
             borderWidth: 3
         }],
